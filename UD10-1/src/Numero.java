3880652e5a562f2a59fdbba1df27803c31ca6f17
Jimxx22 <jaumegil22@gmail.com>
2022-07-18 09:33:34 +0200

    Comentarios

--- UD10-1/src/Numero.java
+++ UD10-1/src/Numero.java
@@ -3,14 +3,17 @@ public class Numero {
 
 	int numero;
 
+	// Constructor que genera número de 1 a 500
 	public Numero() {
-		this.numero = (int) (Math.random()*(499+1))+1;
+		this.numero = (int) (Math.random()*500)+1;
 	}
 
+	// Geter del número
 	public int getNumero() {
 		return numero;
 	}
 	
+	// Método para comparar el número entrado para el usuario y el generado aleatoriamente
 	public int masMenos(int n) {
 		if(n>numero) {
 			return 1;
