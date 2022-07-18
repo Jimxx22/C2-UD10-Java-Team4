a415ceea8674614d312ecf2b668b87d8b3caf281
Jimxx22 <jaumegil22@gmail.com>
2022-07-18 10:04:13 +0200

    Main comentado

--- UD10-2/src/MainApp.java
+++ UD10-2/src/MainApp.java
@@ -3,7 +3,17 @@ import exceptions.ExcepcionCustom;
 public class MainApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		// try catch que lanza una execepción de la clase ExcepcionCustom
+		// Mostrará el mensaje de error establecido en el getMessatge
+		try {
+			System.out.println("Mensaje mostrado por pantalla");
+			throw new ExcepcionCustom();
+		} catch (ExcepcionCustom e) {
+			System.out.println(e.getMessage());
+		}finally {
+			System.out.println("Programa terminado");
+		}
 
 	}
 
