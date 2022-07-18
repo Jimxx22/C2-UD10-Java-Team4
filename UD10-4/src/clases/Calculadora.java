c4b4f6a47615865ec5a3daea5b52a93fcaf89355
Jimxx22 <jaumegil22@gmail.com>
2022-07-18 11:51:42 +0200

    solucionar errores

--- UD10-4/src/clases/Calculadora.java
+++ UD10-4/src/clases/Calculadora.java
@@ -1,6 +1,5 @@
 package clases;
 
-import java.util.InputMismatchException;
 import java.util.Scanner;
 import exceptions.ExceptionCustom;
 
@@ -11,107 +10,115 @@ public class Calculadora {
 	public Calculadora() {}
 	
 	// Métodos para calcular la suma, resta, multiplicación, división, potencia, raíz cuadrada y cúbica
-	public void suma() throws InputMismatchException{
+	public void suma(){
 		try {
-			double n1, n2, res;
+			String n1,n2;
+			double res;
 			System.out.println("Suma");
 			System.out.println("Introduce el primer numero: ");
-			n1 = sc.nextDouble();
+			n1 = sc.nextLine();
 			System.out.println("Introduce el segundo numero: ");
-			n2 = sc.nextDouble();
-			res = n1+n2;
+			n2 = sc.nextLine();
+			res = Double.parseDouble(n1)+Double.parseDouble(n2);
 			System.out.println("El resultado es: "+res);
-		} catch (InputMismatchException ime) { // Miramos si la entrada es un número
+		} catch (NumberFormatException nfe) { // Miramos si la entrada es un número
 			System.out.println("No se ha podido reconocer la entrada del numero");
 		} catch (Exception e) {
-			System.out.println(e.getMessage());
+			System.out.println(e);
 		}
 	}
 	
-	public void resta() throws InputMismatchException{
+	public void resta(){
 		try {
-			double n1, n2, res;
+			String n1,n2;
+			double res;
 			System.out.println("Resta");
 			System.out.println("Introduce el primer numero: ");
-			n1 = sc.nextDouble();
+			n1 = sc.nextLine();
 			System.out.println("Introduce el segundo numero: ");
-			n2 = sc.nextDouble();
-			res = n1-n2;
+			n2 = sc.nextLine();
+			res = Double.parseDouble(n1)-Double.parseDouble(n2);;
 			System.out.println("El resultado es: "+res);
-		}catch (InputMismatchException ime) { // Miramos si la entrada es un número
+		}catch (NumberFormatException nfe) { // Miramos si la entrada es un número
 				System.out.println("No se ha podido reconocer la entrada del numero");
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}	
 	}
 	
-	public void multiplicacion() throws InputMismatchException{
+	public void multiplicacion(){
 		try {
-			double n1, n2, res;
+			String n1,n2;
+			double res;
 			System.out.println("Multiplicación");
 			System.out.println("Introduce el primer numero: ");
-			n1 = sc.nextDouble();
+			n1 = sc.nextLine();
 			System.out.println("Introduce el segundo numero: ");
-			n2 = sc.nextDouble();
-			res = n1*n2;
+			n2 = sc.nextLine();
+			res = Double.parseDouble(n1)*Double.parseDouble(n2);;
 			System.out.println("El resultado es: "+res);
-		}catch (InputMismatchException ime) { // Miramos si la entrada es un número
+		}catch (NumberFormatException nfe) { // Miramos si la entrada es un número
 			System.out.println("No se ha podido reconocer la entrada del numero");
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}	
 	}
 	
-	public void division() throws InputMismatchException, ArithmeticException{
+	public void division(){
 		try {
-			double n1, n2, res;
+			String n1,n2;
+			double res;
 			System.out.println("División");
 			System.out.println("Introduce el primer numero: ");
-			n1 = sc.nextDouble();
+			n1 = sc.nextLine();
 			System.out.println("Introduce el segundo numero: ");
-			n2 = sc.nextDouble();
-			res = n1/n2;
+			n2 = sc.nextLine();
+			res = Double.parseDouble(n1)/Double.parseDouble(n2);;
 			System.out.println("El resultado es: "+res);
 		} catch (ArithmeticException ae) { // Comprobamos que lel divisor no es 0
 			System.out.println("No se puede dividir entre 0");
-		} catch (InputMismatchException ime) { // Miramos si la entrada es un número
+		} catch (NumberFormatException nfe) { // Miramos si la entrada es un número
 			System.out.println("No se ha podido reconocer la entrada del numero");
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}	
 	}
 	
-	public void potencia() throws InputMismatchException{
+	public void potencia(){
 		try {
-			double n1, n2, res;
+			String s1,s2;
+			double res, n1, n2;
 			System.out.println("Potencia");
 			System.out.println("Introduce la base: ");
-			n1 = sc.nextDouble();
+			s1 = sc.nextLine();
 			System.out.println("Introduce el exponente: ");
-			n2 = sc.nextDouble();
+			s2 = sc.nextLine();
+			n1=Double.parseDouble(s1);
+			n2=Double.parseDouble(s2);
 			res =  Math.pow(n1, n2);
 			System.out.println("El resultado es: "+res);
-		} catch (InputMismatchException ime) { // Miramos si la entrada es un número
+		} catch (NumberFormatException nfe) { // Miramos si la entrada es un número
 			System.out.println("No se ha podido reconocer la entrada del numero");
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}	
 	}
 	
-	public void raiz2() throws InputMismatchException, ExceptionCustom{
+	public void raiz2(){
 		try {
-			double n1, res;
+			String n1;
+			double res, n2;
 			System.out.println("Raíz cuadrada");
 			System.out.println("Introduce el numero: ");
-			n1 = sc.nextDouble();
-			
-			if(n1<=0) {
+			n1 = sc.nextLine();
+			n2=Double.parseDouble(n1);
+			if(n2<=0) {
 				throw new ExceptionCustom(111);
 			}
 			
-			res =  Math.sqrt(n1);
+			res =  Math.sqrt(n2);
 			System.out.println("El resultado es: "+res);
-		} catch (InputMismatchException ime) { // Miramos si la entrada es un número
+		} catch (NumberFormatException nfe) { // Miramos si la entrada es un número
 			System.out.println("No se ha podido reconocer la entrada del numero");
 		} catch (ExceptionCustom ec) { // Comprobamos que la entrada es mayor a 0
 			System.out.println(ec.getMessage());
@@ -120,20 +127,21 @@ public class Calculadora {
 		}
 	}
 	
-	public void raiz3() throws InputMismatchException, ExceptionCustom{
+	public void raiz3(){
 		try {
-			double n1, res;
+			String n1;
+			double res, n2;
 			System.out.println("Raíz cúbica");
 			System.out.println("Introduce el numero: ");
-			n1 = sc.nextDouble();
-			
-			if(n1<=0) {
+			n1 = sc.nextLine();
+			n2=Double.parseDouble(n1);
+			if(n2<=0) {
 				throw new ExceptionCustom(111);
 			}
 			
-			res =  Math.cbrt(n1);
+			res =  Math.cbrt(n2);
 			System.out.println("El resultado es: "+res);
-		} catch (InputMismatchException ime) { // Miramos si la entrada es un número
+		} catch (NumberFormatException nfe) { // Miramos si la entrada es un número
 			System.out.println("No se ha podido reconocer la entrada del numero");
 		} catch (ExceptionCustom ec) { // Comprobamos que la entrada es mayor a 0
 			System.out.println(ec.getMessage());
